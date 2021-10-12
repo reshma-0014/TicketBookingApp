0edc37131c21f4a8b7d04606865e192c3ee261a0
reshma <reshmaroyal2002@gmail.com>
2021-10-12 19:11:47 +0530

    initial commit

---
+++ app/src/main/java/com/mycode/ticketbookingapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mycode.ticketbookingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
