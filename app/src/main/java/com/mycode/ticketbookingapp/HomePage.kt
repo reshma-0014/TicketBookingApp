7c70ae262a72f31103c815a40776869071c42576
Priyadharshini0101 <priyadharshinis5102@gmail.com>
2021-10-15 00:22:59 +0530

    Authentication Update

---
+++ app/src/main/java/com/mycode/ticketbookingapp/HomePage.kt
@@ -0,0 +1,13 @@
+package com.mycode.ticketbookingapp
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.homepage)
+    }
+}
