4fbd35732546bf947b05839f129e3a979dfa1876
reshma <reshmaroyal2002@gmail.com>
2021-10-15 13:52:32 +0530

    welcome activit in process

--- app/src/main/java/com/mycode/ticketbookingapp/welcome.kt
+++ app/src/main/java/com/mycode/ticketbookingapp/welcome.kt
@@ -1,14 +1,22 @@
 package com.mycode.ticketbookingapp
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.mycode.ticketbookingapp.databinding.ActivityWelcomeBinding
 
 class welcome : AppCompatActivity() {
-
+    private lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        supportActionBar!!.hide()
+        binding.btngoogle.setOnClickListener(View.OnClickListener { signIn() })
+
     }
+
+    
 }
