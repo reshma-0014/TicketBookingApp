fc24aba192a948d9daa5ad31e6ee23649ed1b137
Priyadharshini0101 <priyadharshinis5102@gmail.com>
2021-10-22 15:51:38 +0530

    Revert "Update"
    This reverts commit 3e60437c

--- app/src/main/java/com/mycode/ticketbookingapp/Signup.kt
+++ app/src/main/java/com/mycode/ticketbookingapp/Signup.kt
@@ -57,10 +57,14 @@ class Signup : AppCompatActivity() {
     private fun savetoFirebaseatabase(username:String,email:String,password:String) {
         val uid=FirebaseAuth.getInstance().uid?: ""
        val ref=FirebaseDatabase.getInstance().getReference("/User/$uid")
-        val user=Users(uid,email,username,"",password)
+        val user=User(username,email,password)
         ref.setValue(user)
             .addOnSuccessListener{
                 Log.d("SignUp","Finally we saved the user to Firebase Database")
             }
         }
+}
+
+class User(val Name:String,val Email:String,val Password:String){
+    constructor():this("","","")
 }
--- app/src/main/java/com/mycode/ticketbookingapp/Users.kt
+++ app/src/main/java/com/mycode/ticketbookingapp/Users.kt
@@ -1,43 +1,30 @@
 package com.mycode.ticketbookingapp
 
-import android.net.Uri
+class Users {
+    var email:String
+    var username:String?
+    var profilepic:String
+
+   // var mail:kotlin.String
+   // var password:kotlin.String
 
 
-//
-//class Users{
-//    var uid:String
-//    var email:String
-//    var username:String
-//    var profilepic:String?
-//    var password:String?
-//
-//    // var mail:kotlin.String
-//   // var password:kotlin.String
-//
-//
-//    constructor(
-//        uid:String,
-//        email: String,
-//        username: String,
-//        profilepic: String?="",
-//        password:String?=""
-//
-//
-//    ) {
-//        this.uid=uid
-//        this.email = email
-//        this.username = username
-//        this.profilepic = profilepic
-//        this.password=password
-//
-//
-//
-//    }
-//
-//}
+    constructor(
+        email: String,
+        username: String?,
+        profilepic: String
 
+       // mail: String,
+        //password: String,
 
+    ) {
+        this.email = email
+        this.username = username
+        this.profilepic = profilepic
 
-class Users(val uid:String, var email:String, var username:String, var profilepic: String?= "", val password:String?="") {
-    constructor():this("","","","")
+        //this.mail = mail
+        //this.password = password
+
+    }
+
 }
